df5037359109d1d2201dc62fa610a325f1022677
rohitsinghme1 <rohitsinghme1@gmail.com>
2025-02-04 06:03:00 +0530

    m1test pratice

---
+++ midterm/app/src/main/java/com/example/midterm/calcu.java
@@ -0,0 +1,30 @@
+package com.example.midterm;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class calcu extends AppCompatActivity {
+    private static final String TAG = "Hello";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_calcu);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Button bnt = find
+        TextView tv1 = findViewById(R.id.tv1);
+        Log.d(TAG, tv1.getText().toString());
+        tv1.setText("rohit");
+    }
+}
